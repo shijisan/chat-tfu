fc6b03ded91e4ebc616c5a626d2847f55229ef8a
shijisan <cjsantos2411638@gmail.com>
2025-06-25 19:13:34 +0800

    fixed revive redis

--- app/api/revive-redis/route.js
+++ app/api/revive-redis/route.js
@@ -1,6 +1,13 @@
 import redis from '@/lib/redis';
 
-export async function POST() {
+export async function POST(req) {
+  const authHeader = req.headers.get('authorization');
+  const token = authHeader?.split(' ')[1];
+
+  if (token !== process.env.APP_API_KEY) {
+    return new Response('Unauthorized', { status: 401 });
+  }
+
   try {
     const pong = await redis.ping();
     return new Response(`Redis is alive: ${pong}`, { status: 200 });
